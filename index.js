b91aaa3d5c663336298b4407087b5a4ec87e7e93
Keijo Kolga <keijo.kolga@gmail.com>
2022-06-04 15:50:29 +0300

    Added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var cors = require('cors');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var apiController = require("./controllers/apiController");
